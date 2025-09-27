31a37d748f8ab33cefc1baaa66d25a41bb482d40
Franz Wilder <gualambo@gmail.com>
2025-09-27 02:33:43 -0500

    Refactor login components for improved styling and functionality; update AuthForm to use custom Input component and add icon support to the login button

--- src/features/auth/components/form.tsx
+++ src/features/auth/components/form.tsx
@@ -2,12 +2,12 @@
 import { use, useState } from "react";
 import useAuth from "../hooks/useAuth";
 import { useRouter } from "next/navigation";
+import Input from "@/components/ui/Input";
+import { LockClosedIcon } from "@heroicons/react/16/solid";
 
 export default function AuthForm() {
   const { email, setEmail, password, setPassword, postLogin, loading, success } = useAuth();
   const router = useRouter();
-
-
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -20,23 +20,22 @@ export default function AuthForm() {
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-      <input
+      <Input
+        text="Email"
         type="email"
-        placeholder="Email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         className="border p-2"
-        required
       />
-      <input
+      <Input
         type="password"
-        placeholder="Password"
+        text="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         className="border p-2"
-        required
       />
-      <button type="submit" className="bg-blue-500 text-white p-2">
+      <button type="submit" className="bg-blue-500 text-white p-2 rounded flex items-center justify-center cursor-pointer hover:bg-blue-600" disabled={loading}>
+        <LockClosedIcon className="h-6 w-6 text-white inline-block mr-2" />
         Login
       </button>
     </form>
