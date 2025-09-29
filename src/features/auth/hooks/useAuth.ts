24649bd3f033dd6de501c37a582f84ffa2c38717
Franz Wilder <gualambo@gmail.com>
2025-09-28 20:26:50 -0500

    Refactor authentication logic; streamline postLogin mutation and remove unused loading and success states

--- src/features/auth/hooks/useAuth.ts
+++ src/features/auth/hooks/useAuth.ts
@@ -1,29 +1,26 @@
 import { useState } from "react";
 import { login } from "../services/auth.api";
 import Swal from "sweetalert2";
+import { useMutation } from "@tanstack/react-query";
 
 export default function useAuth() {
     const [user, setUser] = useState<string | null>(null);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [success, setSuccess] = useState(false);
 
-    const postLogin = async (email: string, password: string) => {
-        setLoading(true);
-        setSuccess(false);
+    const postLogin = useMutation({
+        mutationFn: () => login({ email, password }),
+        onSuccess: (data) => {
+            localStorage.setItem("token", data.data.access_token);
+        },
+        onError: (error: any) => {
+            Swal.fire({
+                title: "Login Failed",
+                text: error.message,
+                icon: "error",
+            });
+        },
+    });
 
-        const data = await login({ email, password });
-        setLoading(false);
-        if (data?.data.access_token) {
-            setUser(data.data.access_token);
-            setSuccess(true);
-            return true;
-        }
-
-        return false;
-
-    };
-
-    return { user, email, setEmail, password, setPassword, postLogin, loading, success };
+    return { user, email, setEmail, password, setPassword, postLogin };
 }
